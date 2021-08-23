1808d0212df74f333be8daab851380e5c6c44b56
rabieifk <farzane.rabiey@gmail.com>
2021-08-23 16:58:02 -0700

    Update Muxn21.v

--- Muxn21.v
+++ Muxn21.v
@@ -1,6 +1,6 @@
 
 
-module mux_module #(parameter N = 8, m = $clog2(N))(inp, select, out);
+module MUX_n-to1 #(parameter N = 8, m = $clog2(N))(inp, select, out);
   input [N-1:0]inp;
   input [m-1:0]select;
   output out; 
@@ -11,9 +11,9 @@ module mux_module #(parameter N = 8, m = $clog2(N))(inp, select, out);
       assign out = (select[0]) ? inp[1] : inp[0]; 
     end
     else begin
-      mux_module #(.N(N/2),.m(m-1)) M1 (.inp(inp[N/2:0]), .select(select[m-2:0]), .out(temp[0]) );
-      mux_module #(.N(N/2),.m(m-1)) M2 (.inp(inp[N-1:N/2]), .select(select[m-2:0]), .out(temp[1]) );
-      mux_module #(.N(2),.m(1)) M3 (.inp(temp), .select(select[m-1]), .out(out));
+      MUX_n-to1 #(.N(N/2),.m(m-1)) M1 (.inp(inp[N/2:0]), .select(select[m-2:0]), .out(temp[0]) );
+      MUX_n-to1 #(.N(N/2),.m(m-1)) M2 (.inp(inp[N-1:N/2]), .select(select[m-2:0]), .out(temp[1]) );
+      MUX_n-to1 #(.N(2),.m(1)) M3 (.inp(temp), .select(select[m-1]), .out(out));
 
     end
   endgenerate
