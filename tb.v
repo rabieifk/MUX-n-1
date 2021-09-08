be56ce15ebe4e6a972263c9516fbc933e413f802
rabieifk <farzane.rabiey@gmail.com>
2021-09-07 18:56:08 -0700

    Update tb.v

--- tb.v
+++ tb.v
@@ -1,34 +1,46 @@
-module tb_mux();
-  reg [13:0] in;
-  reg [3:0] sel;
-  wire out;
+module tb_MUX_w4();
+  reg [3:0] a, b, c, d;
+  reg [1:0] select;
+  wire [3:0] out;
   
-  MUX_n #(.N(14), .m(4) U1 (.inp(in), .select(sel), .out(out));
+  w_bit_N_MUX #(.N(4), .m(2), .W(4)) U1 (.a3(a), .a2(b), .a1(c), .a0(d), .sel(select), .out(out));
   
   initial begin
-    in = 14'b00000000000000;
-    sel = 4'b0000;
-    #20
-    
-    in = 14'b00000000000001;
-    sel = 4'b0000;
-    #20
-    
-    in = 14'b00000000000001;
-    sel = 4'b0001;
-    #20
-    
-    in = 14'b10000000000001;
-    sel = 4'b1101;
-    #20
-    
-    in = 14'b00000100000001;
-    sel = 4'b1001;
-    #20
-    
-    in = 16'b00000001000001;
-    sel = 4'b0101;
+    a = 4'b0000;
+    b = 4'b0000;
+    c = 4'b0001;
+    d = 4'b0000;
+    select = 2'b00;
     #20;
+    
+    a = 4'b0001;
+    b = 4'b0000;
+    c = 4'b0000;
+    d = 4'b0000;
+    select = 2'b00;
+    #20;
+    
+    a = 4'b0000;
+    b = 4'b0001;
+    c = 4'b0000;
+    d = 4'b0001;
+    select = 2'b00;
+    #20;
+    
+    a = 4'b0101;
+    b = 4'b0100;
+    c = 4'b0100;
+    d = 4'b0100;
+    select = 2'b10;
+    #20;
+    
+    a = 4'b0001;
+    b = 4'b1000;
+    c = 4'b0000;
+    d = 4'b1000;
+    select = 2'b11;
+    #20;
+    
     
   end
 endmodule
